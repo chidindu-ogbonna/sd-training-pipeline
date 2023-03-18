c759666c7aa15728fd0701b2a9a62a829b32d814
6ones <promise.bones@gmail.com>
2023-03-18 01:56:51 +0100

    Improve save model to hf function

---
+++ save_model_to_hf.py
@@ -0,0 +1,57 @@
+"""Script to save a model to hf hub.
+
+python save_model_to_hf.py \
+    --model_path "" \
+    --instance_prompt "" \
+    --name_of_concept "" \
+    --instance_data_dir ""
+"""
+
+import argparse
+
+from dotenv import load_dotenv
+
+from utils.hf import save_model_to_hf_hub
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        default=None,
+        type=int,
+        required=True,
+        help="The path to the model to load. Must be a local path.",
+    )
+    parser.add_argument(
+        "--instance_prompt",
+        default=None,
+        type=str,
+        required=True,
+        help="The prompt to use to make inference.",
+    )
+    parser.add_argument(
+        "--name_of_concept",
+        default=None,
+        type=str,
+        required=True,
+        help="The name of the concept you are finetuning. This will be the name of the \
+            model on the HF Hub.",
+    )
+    parser.add_argument(
+        "--instance_data_dir",
+        default=None,
+        type=str,
+        required=True,
+        help="The directory containing the instance data.",
+    )
+    args = parser.parse_args()
+
+    save_model_to_hf_hub(
+        model_path=args.model_path,
+        instance_prompt=args.instance_prompt,
+        name_of_concept=args.name_of_concept,
+        instance_data_dir=args.instance_data_dir,
+    )
--- utils/hf.py
+++ utils/hf.py
@@ -11,7 +11,7 @@ def save_model_to_hf_hub(
     model_path,
     instance_prompt,
     name_of_concept,
-    save_path,
+    instance_data_dir,
 ):
     """Save model to hf hub."""
     HF_TOKEN_WRITE = os.environ.get("HF_TOKEN_WRITE")
@@ -73,7 +73,7 @@ def save_model_to_hf_hub(
         token=HF_TOKEN_WRITE,
     )
     hf_api.upload_folder(
-        folder_path=save_path,
+        folder_path=instance_data_dir,
         path_in_repo="concept_images",
         repo_id=repo_id,
         token=HF_TOKEN_WRITE,
@@ -86,12 +86,12 @@ def save_model_to_hf_hub(
 if __name__ == "__main__":
     name_of_concept = "training-deliberate"
     model_path = "deliberate"
-    save_path = ""
+    instance_data_dir = ""
     instance_prompt = "promise"
 
     save_model_to_hf_hub(
         model_path,
         instance_prompt,
         name_of_concept,
-        save_path,
+        instance_data_dir,
     )
